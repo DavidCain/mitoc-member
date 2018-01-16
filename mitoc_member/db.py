fa8e51fed33285bdafefde46c64b7b3a116db281
David Cain <davidjosephcain@gmail.com>
2018-01-15 22:07:30 -0800

    Finalize route for processing memberships
    Now that we can identify which account needs the update, create
    helper methods for updating the database, and pull these together to
    create a unified route that can do end-to-end processing.
    
    Construct the membership processing piplenie so that it is idempotent.
    This will allow us to easily migrate these routes over to Celery tasks.
    
    (While it's currently unused, this does also contain a similar route for
    checking if a waiver has been processed - we'll be filling that step in
    soon).

--- mitoc_member/db.py
+++ mitoc_member/db.py
@@ -1,6 +1,93 @@
 from .extensions import mysql
 
 
+def get_affiliation(amount):
+    """ There's no CyberSource field for affiliation, so deduce from cost. """
+    # See enum on people_memberships.affiliation
+    return {15: 'student', 20: 'affiliate', 25: 'general'}[int(amount)]
+
+
+def add_person(first, last, email):
+    """ Create a new person in the gear database.
+
+    This is only to be done when we cannot find an existing membership
+    under any known email addresses.
+    """
+    cursor = mysql.connect().cursor()
+    cursor.execute(
+        '''
+        -- Omitted columns (left `null`):
+        -- * phone: Tracked by mitoc-trips, and CyberSource only gives the billing phone
+        -- * affiliation: better tracked by people_memberships OR by mitoc-trips
+        -- * city & state: we've historically not bothered tracking
+        insert into people (firstname, lastname, email, date_inserted)
+        values (%(first)s, %(last)s, %(email)s, now())
+        returning id
+        ''', {'first': first, 'last': last, 'email': email}
+    )
+    return cursor.fetchone()[0]
+
+
+def add_membership(person_id, price_paid, datetime_paid):
+    """ Add a membership payment for an existing MITOC member. """
+    cursor = mysql.connect().cursor()
+    cursor.execute(
+        '''
+        insert into people_memberships
+               (person_id, price_paid, affiliation, date_inserted, expires)
+        values (%(person_id)s, %(price_paid)s, %(affiliation)s, now(),
+                date_add(%(datetime_paid)s, interval 1 year))
+        returning id
+        ''', {'person_id': person_id,
+              'price_paid': price_paid,
+              'affiliation': get_affiliation(price_paid),
+              'datetime_paid': datetime_paid}
+    )
+    return cursor.fetchone()[0]
+
+
+def already_added_waiver(person_id, date_signed):
+    """ Return if this person already has a waiver on this date.
+
+    We want to avoid processing the same waiver twice. Even if the participant
+    signs the waiver twico in one day, it doesn't matter if we insert another
+    record.
+    """
+    cursor = mysql.connect().cursor()
+    cursor.execute(
+        '''
+        select exists(
+          select 1
+            from people_waivers
+           -- date_signed is actually a timestamp
+           where person_id = %(person_id)s
+             and date(date_signed) = %(date_signed)s
+        ) as already_inserted
+        ''', {'person_id': person_id, 'date_signed': date_signed}
+    )
+    return bool(cursor.fetchone()[0])
+
+
+def already_inserted_membership(person_id, date_effective):
+    """ Return if a membership was already created for this day.
+
+    We don't use date_inserted since we could have manually added in a
+    membership with a different date.
+    """
+    cursor = mysql.connect().cursor()
+    cursor.execute(
+        '''
+        select exists(
+          select 1
+            from people_memberships
+           where person_id = %(person_id)s
+             and expires = date_add(%(date_effective)s, interval 1 year)
+        ) as already_inserted
+        ''', {'person_id': person_id, 'date_effective': date_effective}
+    )
+    return bool(cursor.fetchone()[0])
+
+
 def person_to_update(primary_email, all_emails):
     """ Return the person which was most recently updated.
 
--- mitoc_member/public/views.py
+++ mitoc_member/public/views.py
@@ -4,6 +4,7 @@ from flask import Blueprint, current_app, json, request
 
 from mitoc_member.emails import other_verified_emails
 from mitoc_member.signature import SecureAcceptanceSigner
+from mitoc_member import db
 
 blueprint = Blueprint('public', __name__)
 
@@ -24,18 +25,27 @@ def add_membership():
     email = data['req_merchant_defined_data3']  # NOT req_bill_to_email
     primary, all_emails = other_verified_emails(email)
 
+    # Fetch membership, ideally for primary email, but otherwise most recent
+    person_id = db.person_to_update(primary, all_emails)
+    if person_id and db.already_inserted_membership(person_id):
+        return json.jsonify(), 202  # Most likely already processed
+
     # If no membership exists, create one under the primary email
+    if not person_id:
+        first_name = data['req_bill_to_forename']
+        last_name = data['req_bill_to_surname']
+        person_id = db.add_person(first_name, last_name, primary)
 
-    # Translate cost amount to membership level
-    amount = data['req_amount']
-
-    # Add membership
     datetime_paid = datetime.strptime(data['signed_date_time'], "%Y-%m-%dT%H:%M:%SZ")
+    db.add_membership(person_id, data['req_amount'], datetime_paid)
 
-    # Consider firing off an alert about dupes
+    # TODO: Consider firing off an alert if duplicate memberships were detected
     return json.jsonify(), 201
 
 
 @blueprint.route("/waiver", methods=["POST"])
 def add_waiver():
+    """ Process a DocuSign waiver completion.
+
+    Currently, MITOC memberships are all processed through RightSignature. """
     return json.jsonify()
