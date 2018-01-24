cf749f5fd754af07be525f3fd3dd5e58fa317994
David Cain <davidjosephcain@gmail.com>
2018-01-23 16:03:13 -0800

    Install & configure Sentry

--- member/app.py
+++ member/app.py
@@ -1,7 +1,14 @@
+import os
+
 from flask import Flask
+from raven.contrib.flask import Sentry
 
 from member import public
 from member.extensions import mysql
+
+
+RAVEN_DSN = os.getenv('RAVEN_DSN')
+sentry = Sentry(dsn=RAVEN_DSN) if RAVEN_DSN else None
 
 
 def create_app():
@@ -10,4 +17,6 @@ def create_app():
     app.register_blueprint(public.views.blueprint)
 
     mysql.init_app(app)
+    if sentry:
+        sentry.init_app(app)
     return app
