8228abb5010f20f76afdda260ea742de62ad2ff4
David Cain <davidjosephcain@gmail.com>
2018-11-10 20:24:05 -0800

    Use an updated waiver document
    Now that we've updated the structure of the waiver, use a new test XML
    document.

--- tests/test_envelopes.py
+++ tests/test_envelopes.py
@@ -80,23 +80,24 @@ class TestWaiverParser(EnvelopeLoader, unittest.TestCase):
 
     def test_time_signed(self):
         """ The time signed is parsed & converted to UTC. """
-        utc_time_signed = datetime(2018, 2, 22, 0, 29, 7, 147000, tzinfo=timezone.utc)
+        utc_time_signed = datetime(2018, 11, 10, 23, 41, 6, 937000, tzinfo=timezone.utc)
+
         self.assertEqual(self.env.time_signed, utc_time_signed)
 
     @patch('member.envelopes.CompletedEnvelope._get_hours_offset')
     def test_offset(self, hours_offset):
         """ The offset is applied in hours from UTC. """
-        time_signed = datetime(2018, 2, 21, 16, 29, 7, 147000)
+        time_signed = datetime(2018, 11, 10, 18, 41, 6, 937000)
 
         hours_offset.return_value = '0'
         self.assertEqual(time_signed, self.env.time_signed.replace(tzinfo=None))
 
         # Offsets work in both directions
         hours_offset.return_value = '+2'
-        self.assertEqual(datetime(2018, 2, 21, 14, 29, 7, 147000),
+        self.assertEqual(datetime(2018, 11, 10, 16, 41, 6, 937000),
                          self.env.time_signed.replace(tzinfo=None))
         hours_offset.return_value = '-5'
-        self.assertEqual(datetime(2018, 2, 21, 21, 29, 7, 147000),
+        self.assertEqual(datetime(2018, 11, 10, 23, 41, 6, 937000),
                          self.env.time_signed.replace(tzinfo=None))
 
     def test_releasor_email(self):
