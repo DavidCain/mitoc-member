640befdf377d9b7c004b3656e1b7efd19cf56f6a
David Cain <davidjosephcain@gmail.com>
2018-12-22 18:26:05 -0800

    Update to MITOC's new membership dues
    These dues will be going into effect on January 1, 2019.

--- tests/test_db.py
+++ tests/test_db.py
@@ -70,6 +70,23 @@ class TestDbMethods(unittest.TestCase):
             db.add_membership(42, '1.23', datetime.now(), 'NA')
         cursor.execute.assert_not_called()
 
+    @unittest.mock.patch('member.db.get_db')
+    def test_old_payment_values(self, get_db):
+        """ Ensure that the old annual dues are not processed. """
+        cursor = unittest.mock.Mock()
+        get_db.cursor.return_value = cursor
+        old_now_invalid = {
+            'MA': 20,  # MIT affiliates: formerly $20, now $30
+            'NA': 25,  # Non-affiliates: formerly $25, now $40
+            'ML': 25,  # MIT alumni: formerly $25, now $40
+            'NU': 15,  # Non-MIT undergrads: formerly $15, now $40
+            'NG': 15,  # Non-MIT grad stuents: formerly $15, now $40
+        }
+        for code, price in old_now_invalid.items():
+            with self.assertRaises(errors.IncorrectPayment):
+                db.add_membership(42, f'{price}.00', datetime.now(), code)
+        cursor.execute.assert_not_called()
+
     def test_updating_with_invalid_affiliation(self):
         """ We only attempt to update people with valid affiliations. """
         with self.assertRaises(ValueError):
