82e9afb20da6fcf51b1e50b49c8f7d466cad152d
David Cain <davidjosephcain@gmail.com>
2019-08-18 17:04:30 -0700

    Add an extremely simple wsgi test
    Testing a file that's basically a single line of code is a bit
    contrived (especially since the true test is an end-to-end integration
    test that actually spins up a server with WSGI). In any case though,
    this at least gives us 100% coverage on the `wsgi.py` file.

---
+++ tests/test_wsgi.py
@@ -0,0 +1,16 @@
+import unittest
+from unittest import mock
+
+from flask import Flask
+
+from member import app
+
+
+class WsgiTest(unittest.TestCase):
+    def test_exposes_app_in_application_var(self):
+        """ The WSGI script expects an application available. """
+        with mock.patch.object(app, 'create_app', wraps=app.create_app) as create_app:
+            from member import wsgi
+        create_app.assert_called_once()
+        self.assertTrue(isinstance(wsgi.application, Flask))
+        self.assertTrue(hasattr(wsgi.application, 'wsgi_app'))
